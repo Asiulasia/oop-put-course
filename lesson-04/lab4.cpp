9cc937de1a74d01eac2729659f3246d100a927fb
Asiulasia <77675704+Asiulasia@users.noreply.github.com>
2023-03-27 13:04:54 +0200

    Create lab4.cpp

---
+++ lesson-04/lab4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human {
+private:
+	double height;
+	string hair;
+public:
+	Human(double h, string hair) {
+		this->height = h;
+		this->hair = hair;
+	}
+	Human(int h, string hair):Human(static_cast<double>(h), hair) {};
+	Human(float h, string hair) : Human(static_cast<double>(h), hair) {};
+	void info() {
+		cout << height << " cm\n";
+		cout << hair << " color\n";
+	}
+};
+
+
+int main()
+{
+	Human doubleHeight(155.0, "blonde");
+	doubleHeight.info();
+	return 0;
+}
+
+
+
+
+
+
+
+
